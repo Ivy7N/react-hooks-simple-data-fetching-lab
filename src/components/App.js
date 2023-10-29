ea08e506185d9bc1fc6e9f616c4511cdd32d93e1
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-29 19:05:21 +0300

    Completed fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,43 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImageUrl, setDogImageUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchDogImage() {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setDogImageUrl(data.message);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        
+        setIsLoading(false); 
+      }
+    }
+
+    fetchDogImage(); 
+
+  }, []); 
+
+  return (
+    <div className='App'>
+      <h1>Random Dog Image</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <img src={dogImageUrl} alt='A Random Dog' />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
